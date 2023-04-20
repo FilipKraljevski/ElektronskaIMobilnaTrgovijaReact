5fdad4cce3576ed701d611ee7a03820a5933c721
Filip Kraljevski <filipce001@gmail.com>
2023-04-20 10:43:18 +0200

    LV EMT React

---
+++ src/repository/elibraryRepository.js
@@ -0,0 +1,40 @@
+import axios from "../custom-axios/axios";
+
+const ELibraryService = {
+    fetchBooks:() => {
+        return axios.get("/books");
+    },
+    fetchCategories:() => {
+        return axios.get("/categories")
+    },
+    fetchAuthors:() => {
+        return axios.get("/authors")
+    },
+    getBook:(id) => {
+        return axios.get(`/books/${id}`)
+    },
+    editBook:(id, name, category, author, availableCopies) => {
+        return axios.put(`/books/edit/${id}`, {
+            "name":name,
+            "category":category,
+            "author":author,
+            "availableCopies":availableCopies
+        })
+    },
+    deleteBook:(id) => {
+        return axios.delete(`/books/delete/${id}`)
+    },
+    markAsTakenBook:(id) => {
+        return axios.get(`/books/markAsTaken/${id}`)
+    },
+    addBook:(name, category, author, availableCopies) => {
+        return axios.post("books/add", {
+            "name":name,
+            "category":category,
+            "author":author,
+            "availableCopies":availableCopies
+        })
+    }
+}
+
+export default ELibraryService;
